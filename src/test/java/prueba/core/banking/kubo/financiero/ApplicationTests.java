d82992258551f949794e41c31fd4e3ada9f1c92f
Williams Alejandro <walejandromt@gmail.com>
2021-06-12 12:49:54 -0500

    Se agrega configuracion inicial

---
+++ src/test/java/prueba/core/banking/kubo/financiero/ApplicationTests.java
@@ -0,0 +1,13 @@
+package prueba.core.banking.kubo.financiero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
